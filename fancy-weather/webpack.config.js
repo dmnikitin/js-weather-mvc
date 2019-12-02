1a41c89ffbe978146af494162d14fd36c9c002d4
Dmitry <joydnby@gmail.com>
2019-12-02 21:46:28 +0300

    feat: add initial project files

---
+++ fancy-weather/webpack.config.js
@@ -0,0 +1,68 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  target: 'web',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [{
+      test: /\.(js)$/,
+      exclude: /node_modules/,
+      loader: 'babel-loader',
+    },
+    {
+      test: /\.scss$/,
+      use: [{
+        loader: 'style-loader',
+      },
+      {
+        loader: 'css-loader',
+      },
+      {
+        loader: 'sass-loader',
+        options: {
+          sassOptions: {
+            includePaths: ['./node_modules'],
+          },
+        },
+      },
+      ],
+    },
+    {
+      test: /\.(woff(2)?|ttf|otf|eot|svg)(\?v=\d+\.\d+\.\d+)?$/,
+      use: [{
+        loader: 'file-loader',
+        options: {
+          name: '[name].[ext]',
+          outputPath: 'fonts/',
+        },
+      }],
+    },
+    {
+      test: /\.(jpe?g|png|gif|svg)$/i,
+      loader: 'file-loader?name=images/[name].[ext]',
+    },
+    ],
+  },
+  mode: 'production',
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: 'src/index.html',
+    }),
+    new CopyWebpackPlugin([{
+      from: 'src/assets',
+      to: 'assets',
+      ignore: 'index.html',
+    }]),
+    new MiniCssExtractPlugin({
+      filename: 'bundle.css',
+
+    }),
+  ],
+};
