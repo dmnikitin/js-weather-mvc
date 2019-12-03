4300cecafc943b5b34d4069cd3e8fa42f4f40cbe
Dmitry <joydnby@gmail.com>
2019-12-03 22:26:35 +0300

    feat: add basic view component logic

--- fancy-weather/src/classes/view.js
+++ fancy-weather/src/classes/view.js
@@ -1,3 +1,56 @@
+import ControlPanel from './components/controlPanel';
+import WeatherDayPanel from './components/weatherDayPanel';
+import WeatherWeekPanel from './components/weatherWeekPanel';
+import GeoPanel from './components/geoPanel';
+import {
+  createElements,
+  getElement,
+} from '../helpers/other';
+
+const textData = {
+  controlPanel: {},
+  weatherDayPanel: {},
+  weatherWeekPanel: {},
+  geoPanel: {},
+};
+
+
 export default class View {
-  constructor() {}
+  constructor() {
+    // this.controlPanel = new ControlPanel();
+    this.weatherDayPanel = new WeatherDayPanel();
+    // this.weatherWeekPanel = new WeatherWeekPanel();
+    // this.geoPanel = new GeoPanel();
+    const [container, navbar, mainbox] = createElements({
+      element: 'main',
+      classes: 'container',
+    }, {
+      element: 'div',
+      classes: 'navbar',
+    }, {
+      element: 'div',
+      classes: 'mainbox',
+    });
+
+    this.app = getElement('#root');
+    this.container = container;
+    this.navbar = navbar;
+    this.mainbox = mainbox;
+    // <div class="data-loading"><i class='fa fa-refresh fa-spin'></i></div>
+    this.mainbox.append(this.weatherDayPanel.container);
+    // this.mainbox.append(this.controlPanel.container, this.weatherDayPanel.container, this.weatherWeekPanel.container, this.geoPanel.container);
+    this.container.append(this.navbar, this.mainbox);
+    this.app.append(this.container);
+
+    // const skycons = new Skycons({
+    //   color: 'white'
+    // });
+  }
+
+
+  displayData(data, language, temperature, theme) {
+    // const theme = getImage(`weather ${Object.entries(this.loadedData).length === 0 ? this.loadedData.currently.summary : ''}`);
+
+    this.weatherDayPanel.displayData(data);
+  }
 }
