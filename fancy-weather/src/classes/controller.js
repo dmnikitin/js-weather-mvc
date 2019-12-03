6f9dc5bd64fcd45554f44ae78f95a1c03cbc0b54
Dmitry <joydnby@gmail.com>
2019-12-03 23:37:18 +0300

    feat: add controller/model fetch logic

--- fancy-weather/src/classes/controller.js
+++ fancy-weather/src/classes/controller.js
@@ -2,5 +2,21 @@ export default class Controller {
   constructor(model, view) {
     this.model = model;
     this.view = view;
+    this.getInitialData();
   }
+
+  async getInitialData() {
+    try {
+      const position = await this.model.loadInitialData();
+      const result = await this.model.loadData(position.coords.latitude, position.coords.longitude);
+      this.model.loadedData = result;
+    } catch (err) {
+      console.warn(`ERROR(${err.code}): ${err.message}`);
+    }
+    this.view.displayData(this.model.loadedData);
+  }
+
+  // async getData() {
+
+  // }
 }
--- fancy-weather/src/classes/model.js
+++ fancy-weather/src/classes/model.js
@@ -1,5 +1,43 @@
+import {
+  getContentData,
+  getImage,
+} from '../helpers/fetch';
+
 export default class Model {
   constructor() {
+    this.loadedData = {};
+    this.lang = true;
+    this.temperature = true;
+  }
 
+  // getCityFromLatLong(latitiude, longitude){}
+
+  setLang() {
+    this.lang = !this.lang;
+  }
+
+  setTemperature() {
+    this.temperature = !this.temperature;
+  }
+
+  changeTheme() {
+
+  }
+
+  loadInitialData() {
+    return new Promise((resolve, reject) => {
+      const options = {
+        enableHighAccuracy: true,
+        timeout: 5000,
+        maximumAge: 0,
+      };
+      navigator.geolocation.getCurrentPosition(resolve, reject, options);
+    });
+  }
+
+  async loadData(latitude, longitude) {
+    const result = await getContentData(latitude, longitude);
+    this.loadedData = result;
+    return this.loadedData;
   }
 }
