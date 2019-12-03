ede0e6a892bbcac4ae4edad34a3704201c505e25
Dmitry <joydnby@gmail.com>
2019-12-03 22:25:39 +0300

    feat: project setup in index.js

--- fancy-weather/src/index.js
+++ fancy-weather/src/index.js
@@ -1,1 +1,6 @@
 import './styles.scss';
+import View from './classes/view';
+import Model from './classes/model';
+import Controller from './classes/controller';
+
+const app = new Controller(new Model(), new View());
