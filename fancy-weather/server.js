511478294b2ca7feefdbd736abc0068309aada9e
Dmitry <joydnby@gmail.com>
2020-03-24 21:40:20 +0300

    feat: add server request handling

--- fancy-weather/server.js
+++ fancy-weather/server.js
@@ -1,4 +1,6 @@
-const { API_KEY } = process.env;
+const {
+  weatherToken, geoToken, imageToken,
+} = process.env;
 const fetch = require('node-fetch');
 const path = require('path');
 const express = require('express');
@@ -8,31 +10,62 @@ const port = process.env.PORT || 8080;
 const dir = path.join(__dirname, 'public');
 const app = express();
 const cors = require('cors');
-// let url, longitude, latitude;
 
-// app.use(cors())
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.static(dir));
+app.use(cors());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static(dir));
 
-// app.get('/loadserver', (req, res) => {
-//     res.send('<h1>server running</h1>');
-// });
+app.get('/loadserver', (req, res) => {
+  res.send('<h1>server running</h1>');
+});
 
-// app.post('/', (req, res) => {
-//     latitude = req.body.latitude;
-//     longitude = req.body.longitude;
-//     url = `https://api.darksky.net/forecast/${API_KEY}/${latitude},${longitude}`;
-//     if (!latitude || !longitude) {
-//         res.redirect('/error');
-//     }
 
-//     fetch(url).then(response => {
-//             if (response.ok) {
-//                 return response.json();
-//             }
-//             throw new Error('failed')
-//         }, networkError => console.log(networkError.message))
-//         .then(jsonResponse => res.json(jsonResponse))
-// });
+app.post('/weather', (req, res) => {
+  const { latitude, longitude, language } = req.body;
+  const url = `https://api.darksky.net/forecast/${weatherToken}/${latitude},${longitude}?lang=${language}`;
+  if (!latitude || !longitude) {
+    res.redirect('/error');
+  }
+
+  fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('failed');
+  }, (networkError) => console.log(networkError.message))
+    .then((jsonResponse) => res.json(jsonResponse));
+});
+
+
+app.post('/image', (req, res) => {
+  const { query } = req.body;
+  const url = `https://api.unsplash.com/photos/random?query=${query}&client_id=${imageToken}`;
+  if (!query) {
+    res.redirect('/error');
+  }
+  fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('failed');
+  }, (networkError) => console.log(networkError.message))
+    .then((jsonResponse) => res.json(jsonResponse));
+});
+
+app.post('/place', (req, res) => {
+  const { query, language } = req.body;
+  const url = `https://api.opencagedata.com/geocode/v1/json?q=${query}&key=${geoToken}&language=${language}&pretty=1`;
+  if (!query) {
+    res.redirect('/error');
+  }
+  fetch(url).then((response) => {
+    if (response.ok) {
+      return response.json();
+    }
+    throw new Error('failed');
+  }, (networkError) => console.log(networkError.message))
+    .then((jsonResponse) => res.json(jsonResponse));
+});
+
 
 app.listen(port, () => console.log(`Listening on http://localhost:${port}/`));
