c12e5486f17f22530707cf71f7620ea148648c46
dmitry <joydnby@gmail.com>
2020-06-03 18:25:02 +0300

    fix: geolocation issues for minor browsers

--- src/index.js
+++ src/index.js
@@ -2,5 +2,6 @@ import './styles.scss';
 import View from './classes/view';
 import Model from './classes/model';
 import Controller from './classes/controller';
+import * as geolocation from './geolocation';
 /* eslint no-unused-vars: 0 */
 const app = new Controller(new Model(), new View());
