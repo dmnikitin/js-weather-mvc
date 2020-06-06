bb37d2186f40b3988c208eddcf0f9bc9dcad6d60
dmitry <joydnby@gmail.com>
2020-06-06 11:02:45 +0300

    refactor: main server file

--- server.js
+++ server.js
@@ -2,8 +2,11 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const path = require('path');
+const errorMidlleware = require('./server/middlewares/error');
+const loggerMiddleware = require('./server/middlewares/logger');
 const router = require('./server/routes');
-const { PORT } = require('./server/config');
+const { PORT } = require('./server/common/config');
+const logger = require('./server/common/winston');
 
 const app = express();
 const dir = path.join(__dirname, 'build');
@@ -11,7 +14,19 @@ const dir = path.join(__dirname, 'build');
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(dir));
+
+app.use(loggerMiddleware);
 app.use('/', router);
+app.use(errorMidlleware);
+
+process.on('uncaughtException', (err) => {
+  logger.error({ statusCode: 500, message: err.message });
+  process.exit(1);
+});
+
+process.on('unhandledRejection', (reason) => {
+  logger.error({ statusCode: 500, message: reason.message });
+});
 
 // eslint-disable-next-line no-console
 app.listen(PORT, () => console.log(`Listening on http://localhost:${PORT}/`));
